91737f04036b93862bed73993401fcf2f8ff5816
galiev <ggvalion@gmail.com>
2024-07-20 15:23:11 +0300

    DTO fix

--- main.py
+++ main.py
@@ -39,9 +39,11 @@ class Cloth(BaseModel):
 
 
 class Outfit(BaseModel):
+    id: int
     name: str
     description: str
-    image: str
+    image: int
+    clothes: List[int]
 
 
 class ImageData(BaseModel):
